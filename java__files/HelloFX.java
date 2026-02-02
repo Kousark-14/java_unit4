b88d291fef8ebfac6ab014fd1148547a29a46248
KOUSAR K <sheikhkauser14@gmail.com>
2026-02-02 14:48:05 +0530

    Create HelloFX.java

---
+++ java__files/HelloFX.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+public class HelloFX extends Application {
+@Override
+public void start(Stage stage) {
+Label label = new Label("Welcome to JavaFX");
+Scene scene = new Scene(label, 300, 200);
+stage.setTitle("JavaFX Demo");
+stage.setScene(scene);
+stage.show();
+}
+public static void main(String[] args) {
+launch(args);
+}
+}
